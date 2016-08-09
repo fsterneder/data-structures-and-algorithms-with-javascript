b35b95a7ecf17f7cda4e9f3628b10d513885544c
fsterneder <florian@sterneder.net>
2016-08-09 13:29:50 +0200

    9.8.16 - morning

--- notes/chap3.js
+++ notes/chap3.js
@@ -1,14 +1,25 @@
 function List() {
-   this.listSize = 0;
-   this.dataStore = [];
-   this.append = append;
-}
+  this.dataStore = [];
+  this.append = append;
+  this.remove = remove;
+  this.length = ()=>this.dataStore.length;
+  this.print = () => console.log(this.dataStore);
 
-function append(element) {
-   this.dataStore[this.listSize++] = element;
+  function append(element) {
+    this.dataStore[this.dataStore.length] = element;
+  }
+  function remove(element){
+    if(this.dataStore.indexOf(element) > -1){
+      this.dataStore.splice(this.dataStore.indexOf(element));
+    } else {
+      console.log('ERROR: no matching entry in list');
+    }
+  }
 }
 
 var names = new List();
+
 names.append("Cynthia");
 names.append("Raymond");
 names.append("Barbara");
+names.print();
