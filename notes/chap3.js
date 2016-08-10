4207039c3ce6de12a7043acd42d82ada018880e1
fsterneder <florian@sterneder.net>
2016-08-10 10:45:57 +0200

    2016-08-10 morning

--- notes/chap3.js
+++ notes/chap3.js
@@ -1,13 +1,14 @@
 function List() {
   this.dataStore = [];
+
   this.append = (element) => this.dataStore[this.dataStore.length] = element;
   this.remove = (element) => this.dataStore.splice(this.find(element),1);
+  this.insertAfter = (element,after) => this.dataStore.splice(this.find(after)+1,0,element);
+  this.clear = () => this.dataStore = [];
+
   this.length = () => this.dataStore.length;
   this.find = (element) => this.dataStore.indexOf(element) > -1 ? this.dataStore.indexOf(element) : (function(){throw Error('yes');})();
   this.print = () => console.log(this.dataStore);
-
-  function insert(element,after){    
-  }
 }
 
 var names = new List();
@@ -19,5 +20,7 @@ names.append('Slice');
 names.print();
 names.remove('Ray');
 names.print();
-//names.remove('Raymano');
+names.insertAfter('Ray','Cynthia');
+names.print();
+names.clear();
 names.print();
