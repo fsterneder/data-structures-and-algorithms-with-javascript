e56ed3f35ea8519821413122241249bc52539a21
fsternder <florian@sterneder.net>
2016-08-17 14:56:30 +0200

    72 - Demonstrating Recursion

--- notes/stacks.js
+++ notes/stacks.js
@@ -11,6 +11,21 @@ class Stack {
   }
 }
 
-let names = new Stack()
+// Multiple Base Conversions
+function multiBase(n,b){
+  let s = new Stack()
+  while(n > 0){
+  s.push(n%b)
+  n = Math.floor(n /= b)
+  }
+  return s.data.reverse().join('')
+}
 
-debugger
+// Palindrome Check
+function isPalindrome(word){
+  let s = new Stack()
+  for(let i of word){s.push(i)}
+  let rev = '', leng = s.top()
+  for(let i = 0;i < leng;i++){rev += s.pop()}
+  return word == rev ? true : false
+}
