e0e42e26e96a98a4794a34588f550287239206a3
fsterneder <florian@sterneder.net>
2016-08-23 09:22:19 +0200

    86 - Priority Queues

--- queues.js
+++ queues.js
@@ -12,3 +12,21 @@ class Queue {
 }
 
 module.exports = Queue
+
+function radixSort(toSort){
+  let bin = []
+  bin.toString = () => bin.map(a => a.data).join('|').trim()
+  for(let i=0;i<10;i++){bin[i] = new Queue}
+  for(let i of toSort){bin[i%10].enqueue(i)}
+  let lenMap = bin.map(a => a.length())
+  for(let i=0;i<10;i++){
+    for(let j=0;j<lenMap[i];j++){
+      let look = bin[i].dequeue()
+      bin[Math.floor(look/10)].enqueue(look)
+    }
+  }
+  console.log(bin.toString())
+}
+
+//radixSort([22,23,95,45,48,99,92,20,01,34,59,23,11,56,62,91])
+radixSort([45,72,93,51,21,16,70,41,27,31])
