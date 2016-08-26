9c1f8d2c3c0b7a2d99d106a71d316cbcda5d1fa9
fsterneder <florian@sterneder.net>
2016-08-26 12:17:07 +0200

    97 - Doubly Linked Lists

---
+++ linked-lists.js
@@ -0,0 +1,85 @@
+// Linked List
+
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = new Node('head')
+    this.find = (target) => {
+      function find(target,node) {
+        if(node.element == target) {return node}
+        else if(node.next == null) {return null}
+        else {return find(target,node.next)}
+      }
+      return find(target,this.head)
+    }
+    this.findBefore = (target) => {
+      function findBefore(target,node){
+        if(node.next.element == target) {return node}
+        else if(node.next == null) {return null}
+        else {return findBefore(target,node.next)}
+      }
+      return findBefore(target,this.head)
+    }
+    this.findLast = () => {
+      function findLast(node) {
+        if(node.next == null) {return node}
+        else {return findLast(node.next)}
+      }
+      return findLast(this.head)
+    }
+    this.insert = (element) => {
+      this.findLast().next = new Node(element)
+    }
+    this.insertBefore = (target,element) => {
+      try {
+      let node = new Node(element)
+      let targ = this.find(target)
+      this.findBefore(target).next = node
+      node.next = targ
+      return true
+      } catch(e) {
+        return false
+      }
+    }
+    this.remove = (element) => {
+      try{
+        let toDelete = this.find(element)
+        this.findBefore(element).next = toDelete.next
+        return true
+      } catch(e){
+        return false
+      }
+    }
+    this.display = () => {
+      let outp = []
+      function display(node) {
+        if(node.element !== 'head') {
+          outp.push((outp.length + 1) + ") " + node.element) 
+        }
+        if(node.next == null){
+          return outp
+        }
+        else {return display(node.next)}
+      }
+      return display(this.head).join('\n')
+    }
+  }
+}
+
+module.exports = LinkedList
+
+// test only
+var a = new LinkedList()
+a.insert('first')
+a.insert('second')
+a.insert('third')
+a.insert('fifth')
+console.log(a.display())
+
+debugger
