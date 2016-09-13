8526f0cbefed26f17fc538b050f54ed7e91c63ad
fsternder <florian@sterneder.net>
2016-09-13 12:51:32 +0200

    140 - Building a Binary Search Tree Implementation

---
+++ binary-tree.js
@@ -0,0 +1,10 @@
+// Binary Tree & Binary Search Tree
+
+class Node {
+  constructor(data,left,right){
+    this.data = data
+    this.left = left
+    this.right = right
+  }
+  show(){return this.data}
+}
