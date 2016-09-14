108416edb87628d56b0924773892d9766dfaffc4
fsterneder <florian@sterneder.net>
2016-09-14 14:09:16 +0200

    148 - Removing Nodes from a BST

--- binary-tree.js
+++ binary-tree.js
@@ -1,10 +1,122 @@
 // Binary Tree & Binary Search Tree
 
 class Node {
-  constructor(data,left,right){
+  constructor(data,left=null,right=null){
     this.data = data
     this.left = left
     this.right = right
   }
-  show(){return this.data}
 }
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null
+  }
+  insert(data){
+    let insertNode = new Node(saneInput(data))
+    if(this.root === null){return this.root = insertNode}
+    else{return traverseBST(this.root)}
+
+    function traverseBST(currNode){
+      if(insertNode.data < currNode.data){
+        if(currNode.left === null){return currNode.left = insertNode}
+        else {return traverseBST(currNode.left)}
+      }
+      else if(insertNode.data > currNode.data){
+        if(currNode.right === null){return currNode.right = insertNode}
+        else {return traverseBST(currNode.right)}
+      }
+      else {return false}
+    }
+    function saneInput(data){
+      switch (typeof data) {
+        case 'string':
+          !Number.isNaN(Number.parseInt(data)) ?
+          data = Number.parseInt(data) :
+          data = data.split('').map(a => a.charCodeAt(0)).reduce((b,c) => b+c)
+          break;
+        case 'number':
+          break;
+        default:
+          throw new Error('wrong data type')
+      }
+      return data
+    }
+  }
+  showOrder(){
+    let outp = []
+    showInOrder(this.root)
+    return outp
+
+    function showInOrder(node){
+      if(node !== null){
+        showInOrder(node.left)
+        outp.push(node.data)
+        showInOrder(node.right)
+      }
+    }
+  }
+  showPre(){
+    let outp = []
+    showPreOrder(this.root)
+    return outp
+
+    function showPreOrder(node){
+      if(node !== null){
+        outp.push(node.data)
+        showPreOrder(node.left);showPreOrder(node.right)
+      }
+    }
+  }
+  showPost(){
+    let outp = []
+    showPostOrder(this.root)
+    return outp
+
+    function showPostOrder(node){
+      if(node !== null){
+        showPostOrder(node.left);showPostOrder(node.right)
+        outp.push(node.data)
+      }
+    }
+  }
+  min(){
+    return getMinLeaf(this.root)
+
+    function getMinLeaf(node){
+      if(node.left == null){return node}
+      else {return getMinLeaf(node.left)}
+    }
+  }
+  max(){
+    return getMaxLeaf(this.root)
+
+    function getMaxLeaf(node){
+      if(node.right == null){return node}
+      else {return getMaxLeaf(node.right)}
+    }
+  }
+  find(target){
+    debugger
+    return searchBST(this.root)
+    
+    function searchBST(node){
+      if(node !== null){
+        if(node.data === target){return node}
+        else {
+          if(node.data > target){return searchBST(node.left)}
+          else if(node.data < target){return searchBST(node.right)}
+          else {return false}
+        }
+      }
+    }
+  }
+}
+
+// TP
+let a = new BinarySearchTree()
+a.insert(50)
+a.insert(12)
+a.insert(3)
+a.find(3)
+debugger
